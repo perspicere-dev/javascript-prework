f89e82f81f4988ced2f5e3e3416d8c1ee8d168be
Przemysław Wnęk <wnek.przemek@gmail.com>
2021-03-31 13:45:41 +0200

    add JS playerMove

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function printMessage(msg){
+	let div = document.createElement('div');
+	div.innerHTML = msg;
+	document.getElementById('messages').appendChild(div);
+}
+
+function clearMessages(){
+	document.getElementById('messages').innerHTML = '';
+}
+let playerMove = `papier`;
+
+printMessage('Zagrałem kamień! Jeśli Twój ruch to ' + playerMove +', to wygrywasz!');
