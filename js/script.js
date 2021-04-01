cd59e9b0092a407ea032d69cf1782ea4384bd5f1
Przemysław Wnęk <wnek.przemek@gmail.com>
2021-04-01 16:41:08 +0200

    add functions.js and some js

--- js/script.js
+++ js/script.js
@@ -2,13 +2,15 @@ let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Wylosowana liczba to: ' + randomNumber);
 
-if (randomNumber == 1) {
+let computerMove = getMoveName(randomNumber);
+
+/*if (randomNumber == 1) {
   computerMove = 'kamień';
 } else if (randomNumber == 2) {
   computerMove = 'papier';
 } else if (randomNumber == 3) {
   computerMove = 'nożyce';
-}
+}*/
 
 printMessage('Mój ruch to: ' + computerMove); //czy to ma zostać?
 
@@ -20,9 +22,9 @@ let playerMove = 'nieznany ruch';
 
 if (playerInput == '1') {
   playerMove = 'kamień';
-} else if (randomNumber == 2) {
+} else if (playerInput == 2) {
   playerMove = 'papier';
-} else if (randomNumber == 3) {
+} else if (playerInput == 3) {
   playerMove = 'nożyce';
 }
 
