4687ea1676946aa5a64987c5973bf35af0e96fea
Przemysław Wnęk <wnek.przemek@gmail.com>
2021-04-01 13:13:36 +0200

    remove nieznany ruch for computer

--- js/script.js
+++ js/script.js
@@ -2,16 +2,24 @@ let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Wylosowana liczba to: ' + randomNumber);
 
-let computerMove = 'nieznany ruch';
-
-if(randomNumber == 1){
+if (randomNumber == 1) {
   computerMove = 'kamień';
-}
-if(randomNumber == 2){
+} else if (randomNumber == 2) {
   computerMove = 'papier';
-}
-if(randomNumber == 3){
+} else if (randomNumber == 3) {
   computerMove = 'nożyce';
 }
 
 printMessage('Mój ruch to: ' + computerMove);
+
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+}
+
+printMessage('Twój ruch to: ' + playerMove);
