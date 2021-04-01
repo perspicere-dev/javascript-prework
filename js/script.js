5657dcdca505e5dc157bc2df31656edf68abcc7c
Przemysław Wnęk <wnek.przemek@gmail.com>
2021-04-01 22:40:49 +0200

    simplify last if

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Wylosowana liczba to: ' + randomNumber);
 
+let computerMove = 'nieznany ruch';
+
 if (randomNumber == 1) {
   computerMove = 'kamień';
 } else if (randomNumber == 2) {
@@ -18,7 +20,7 @@ console.log('Gracz wpisał: ' + playerInput);
 
 let playerMove = 'nieznany ruch';
 
-if (playerInput == '1') {
+if (playerInput == 1) {
   playerMove = 'kamień';
 } else if (playerInput == 2) {
   playerMove = 'papier';
@@ -29,21 +31,13 @@ if (playerInput == '1') {
 printMessage('Twój ruch to: ' + playerMove); //czy to ma zostać?
 
 if (computerMove == 'kamień' && playerMove == 'papier') {
-  printMessage('Ty wygrywasz!');
-} else if (computerMove == 'kamień' && playerMove == 'nożyce') {
-  printMessage('Przegrywasz!');
-} else if (computerMove == 'papier' && playerMove == 'kamień') {
-  printMessage('przegrywasz!');
+  printMessage('Ty wygrywaszss!');
 } else if (computerMove == 'papier' && playerMove == 'nożyce') {
-  printMessage('wygrywasz!');
+  printMessage('Ty wygrywaszss!');
 } else if (computerMove == 'nożyce' && playerMove == 'kamień') {
-  printMessage('wygrywasz!');
-} else if (computerMove == 'nożyce' && playerMove == 'papier') {
-  printMessage('przegrywasz!');
-} else if (computerMove == 'kamień' && playerMove == 'kamień') {
-  printMessage('remis!');
-} else if (computerMove == 'papier' && playerMove == 'papier') {
-  printMessage('remis!');
-} else if(computerMove == 'nożyce' && playerMove == 'nożyce') {
-  printMessage('remis!');
+  printMessage('Ty wygrywaszsss!');
+} else if (computerMove == playerMove) {
+  printMessage('Remissss!');
+} else {
+  printMessage('Przegrywasz!');
 }
