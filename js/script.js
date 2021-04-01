15d5b8057da60cad6232ff353bbfdfaa9d9fae1e
Przemysław Wnęk <wnek.przemek@gmail.com>
2021-04-01 13:55:46 +0200

    ADD all rules &&

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ if (randomNumber == 1) {
   computerMove = 'nożyce';
 }
 
-printMessage('Mój ruch to: ' + computerMove);
+printMessage('Mój ruch to: ' + computerMove); //czy to ma zostać?
 
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
@@ -26,10 +26,24 @@ if (playerInput == '1') {
   playerMove = 'nożyce';
 }
 
-printMessage('Twój ruch to: ' + playerMove);
+printMessage('Twój ruch to: ' + playerMove); //czy to ma zostać?
 
 if (computerMove == 'kamień' && playerMove == 'papier') {
   printMessage('Ty wygrywasz!');
 } else if (computerMove == 'kamień' && playerMove == 'nożyce') {
-  printMessage('Ty wygrywasz!');
+  printMessage('Przegrywasz!');
+} else if (computerMove == 'papier' && playerMove == 'kamień') {
+  printMessage('przegrywasz!');
+} else if (computerMove == 'papier' && playerMove == 'nożyce') {
+  printMessage('wygrywasz!');
+} else if (computerMove == 'nożyce' && playerMove == 'kamień') {
+  printMessage('wygrywasz!');
+} else if (computerMove == 'nożyce' && playerMove == 'papier') {
+  printMessage('przegrywasz!');
+} else if (computerMove == 'kamień' && playerMove == 'kamień') {
+  printMessage('remis!');
+} else if (computerMove == 'papier' && playerMove == 'papier') {
+  printMessage('remis!');
+} else(computerMove == 'nożyce' && playerMove == 'nożyce') {
+  printMessage('remis!');
 }
