a60b4801efb3b696f8d96d441080dcc75cb1df9a
Przemysław Wnęk <wnek.przemek@gmail.com>
2021-04-02 19:02:55 +0200

    Add function getMoveName & isplayResult

--- js/script.js
+++ js/script.js
@@ -1,43 +1,44 @@
 let randomNumber = Math.floor(Math.random() * 3 + 1);
-
 console.log('Wylosowana liczba to: ' + randomNumber);
 
-let computerMove = 'nieznany ruch';
+let computerMove = getMoveName(randomNumber);
+printMessage('Mój ruch to: ' + computerMove);
 
-if (randomNumber == 1) {
-  computerMove = 'kamień';
-} else if (randomNumber == 2) {
-  computerMove = 'papier';
-} else if (randomNumber == 3) {
-  computerMove = 'nożyce';
+function getMoveName(argMoveId) {
+  console.log('Wywołano funkcję getMoveName z argumentem argMoveId: ' + argMoveId);
+  if (argMoveId == 1) {
+    return 'kamień';
+  } else if (argMoveId == 2) {
+    return 'papier';
+  } else if (argMoveId == 3) {
+    return 'nożyce';
+  } else {
+    printMessage('Nie ma takiego ruchu jak "' + argMoveId + '"!');
+  }
 }
-
-printMessage('Mój ruch to: ' + computerMove); //czy to ma zostać?
 
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
 console.log('Gracz wpisał: ' + playerInput);
 
-let playerMove = 'nieznany ruch';
 
-if (playerInput == 1) {
-  playerMove = 'kamień';
-} else if (playerInput == 2) {
-  playerMove = 'papier';
-} else if (playerInput == 3) {
-  playerMove = 'nożyce';
-}
+let playerMove = getMoveName(playerInput);
 
-printMessage('Twój ruch to: ' + playerMove); //czy to ma zostać?
+printMessage('Twój ruch to: ' + playerMove);
+displayResult(playerMove, computerMove); // do tego nie doszedłem sam. Z opisu modułu bym tego nie zrobił. Podejrzałem w necie.
+                                
 
-if (computerMove == 'kamień' && playerMove == 'papier') {
-  printMessage('Ty wygrywaszss!');
-} else if (computerMove == 'papier' && playerMove == 'nożyce') {
-  printMessage('Ty wygrywaszss!');
-} else if (computerMove == 'nożyce' && playerMove == 'kamień') {
-  printMessage('Ty wygrywaszsss!');
-} else if (computerMove == playerMove) {
-  printMessage('Remissss!');
-} else {
-  printMessage('Przegrywasz!');
+function displayResult(argComputerMove, argPlayerMove) {
+  console.log('wykonano ruchy. Komputer:', argComputerMove,'. Gracz:', argPlayerMove);
+  if (argComputerMove == 'kamień' && argPlayerMove == 'papier') {
+    printMessage('Ty wygrywasz!');
+  } else if (argComputerMove == 'papier' && argPlayerMove == 'nożyce') {
+    printMessage('Ty wygrywasz!');
+  } else if (argComputerMove == 'nożyce' && argPlayerMove == 'kamień') {
+    printMessage('Ty wygrywasz!');
+  } else if (argComputerMove == argPlayerMove) {
+    printMessage('Remis!');
+  } else {
+    printMessage('Przegrywasz!');
+  }
 }
